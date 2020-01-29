0e029ffa28c525365ebdf3b2e6ae5c8b21191c74
Vivek Shah <34334421+vivekshah1801@users.noreply.github.com>
2020-01-29 12:44:53 +0530

    Update RSA-Vivek.c

--- RSA-Vivek.c
+++ RSA-Vivek.c
@@ -34,9 +34,7 @@ long long fast_expo_iter(long long x,long long y,long long m)
 		y = y>>1; // y = y/2;
 	}
 	return res;
-}
-
-int rabin_miller_primality(long long n,int a)
+}int rabin_miller_primality(long long n,int a)
 {
 	long long m,k;
 	
@@ -50,10 +48,18 @@ int rabin_miller_primality(long long n,int a)
 			if(!(n1&1))
 			{
 				k++;
-				n1 = n1>>1;
+				n1 >>= 1;
+				//printf("\n%lld",n1); 
 			}
 			else
-				m = n1>>k;
+			{	
+			
+				int k1 = 1<<k;
+				//printf("%d",k1);
+				m = (n-1)/k1;
+				//printf("*M = %d",m);
+				break;
+			}
 		}
 	}
 	else
@@ -61,18 +67,51 @@ int rabin_miller_primality(long long n,int a)
 		m = n-1;
 		k = 0;
 	}
-	
-	
+
 	long long t;
-	t = fast_expo_iter(a,m,n)
-	if(t==1 || t==-1)
+	t = fast_expo(a,m,n);
+	if(t==1 || t==(n-1))
 		return 1;
 	
-	for(int i=0;i<k-1;i++)
+	int i;
+	for(i=0;i<k-1;i++)
 	{
-		t = (t*t)%m;
+		t = (t*t)%n;
 		if(t==1) return 0;
-		else if(t==-1) return 1;
+		else if(t==(n-1)) return 1;
 	}
 	return 0;
+}
+
+long long generate_prime()
+{
+	long long p=0;
+	while(1)
+	{
+		p = rand()/10000;
+		if(rabin_miller_primality(p,2)==1)
+			return p;
+	}
+}
+
+long long generate_prime2(long long p)
+{
+	long long q=0;
+	while(1)
+	{
+		q = rand()/10000;
+		if(rabin_miller_primality(q,2)==1)
+			if(p!=q)
+				return q;
+	}
+}
+
+
+long long rsa()
+{
+	long long p = generate_prime();
+	long long q = generate_prime2(p);
+	printf("p = %lld, q = %lld, randmax = %lld", p,q, RAND_MAX);
+	long long n = p*q;
+	printf("n = %d",n);
 }
